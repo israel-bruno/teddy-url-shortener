abda55fac41a95676d94d14b7fbc1a6c94e80784
Israel Morais <ibrmorais@viptech.com.br>
2024-08-29 18:22:25 -0300

    feat: add fastify

--- apps/urls/src/main.ts
+++ apps/urls/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
 import { UrlsModule } from './urls.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(UrlsModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    UrlsModule,
+    new FastifyAdapter(),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
